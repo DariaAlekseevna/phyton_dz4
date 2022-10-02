d34c800fed27f43878923fb70c54c4522ade758e
Daria Alekseevna <dasha.gugnina98@gmail.com>
2022-10-02 08:24:27 +0300

    решение задачи 1

--- task1.py
+++ task1.py
@@ -1,4 +1,16 @@
 # Вычислить число c заданной точностью d
-
 # Пример:
 # при $d = 0.001, π = 3.141
+
+import math
+
+
+
+d = str(input("введите точность числа пи: "))
+
+d = len(str(d).split('.')[1])
+pi = math.pi
+print(pi)
+st_pi = str(round(pi, d+1))
+l = len(st_pi) - 1
+print(st_pi.replace(st_pi[l], ''))
